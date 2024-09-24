a740035145c23b896f00e69484c3c8b7c2159503
Dassh01 <gilfixbea@gmail.com>
2024-09-24 00:16:31 -0700

    Added a configurable image on hover transition

--- script.js
+++ script.js
@@ -1,12 +1,32 @@
+const hoverImage = document.getElementById('cat-image');
+const tickrate = 250 //Control in millis how fast the transition goes
+
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
 
-let outputs = ['MEOW MEOW MEOW','boo!!!','hi']
+let outputs = [':3c','boo!!!','hi']
 
 document.getElementById('change-text-button').addEventListener('click', function() {
 
     let output = outputs[getRandomInt(outputs.length)]
     
     document.getElementById('main-text').textContent = output;
+});
+
+//The "transition" works by modifying the filepath field of the hoverImage "object?"
+hoverImage.addEventListener('mouseover', function() {
+    hoverImage.style.opacity = 0; //Start fading out
+    setTimeout(() => {
+        hoverImage.src = 'fishcat.png';
+        hoverImage.style.opacity = 1; //Start fading in
+    }, tickrate);
+});
+
+hoverImage.addEventListener('mouseout', function() {
+    hoverImage.style.opacity = 0; //Start fading out
+    setTimeout(() => {
+        hoverImage.src = 'pfp.png';
+        hoverImage.style.opacity = 1; //Start fading in
+    }, tickrate); 
 });
